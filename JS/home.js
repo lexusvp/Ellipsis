f8ebeda7575b5e275cbe1786764ca766e92e312b
Vazn <cavazzoni.chris@gmail.com>
2021-12-18 22:06:50 +0100

    Minor changes

--- JS/home.js
+++ JS/home.js
@@ -2,13 +2,15 @@
    const triggElement = document.querySelector("main > div");
    const nav = document.querySelector("nav");
    const chat = document.querySelector("#chat_container");
+
+   chat.style.bottom = "15px";
    window.addEventListener("scroll", () => {
       if (window.scrollY > triggElement.offsetTop) {
          nav.style.bottom = "0px";
          chat.style.bottom = "70px";
       } if (window.scrollY + window.innerHeight < triggElement.offsetTop) {
-         chat.style.bottom = "15px";
          nav.style.bottom = "";
+         chat.style.bottom = "15px";
       }
    })
 })();
