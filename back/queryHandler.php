721202c8c1708e80e4aa2a76b505b021164482df
Vazn <86126150+Vazn@users.noreply.github.com>
2022-01-24 16:10:44 +0100

    Minor bugs

--- back/queryHandler.php
+++ back/queryHandler.php
@@ -1,6 +1,6 @@
 <?php
    function queryDatabase($prepared, $queryArgs = NULL) { 
-      $dsn = "mysql:host=localhost;dbname=task";
+      $dsn = "mysql:host=localhost;dbname=ellipsis";
 
       $database = new PDO($dsn, "root", "root", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
       $database -> exec("set names utf8");
